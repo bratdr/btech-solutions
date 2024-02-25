1cbd3975d65aa4a8544fb178ff932eef95626b45
Brata Darmawan <bratadarmawan@gmail.com>
2024-02-26 04:57:36 +0700

    added Link in Image

--- app/components/navigation/index.jsx
+++ app/components/navigation/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
     <>
       <div className="fixed flex flex-row items-center justify-between px-6 w-full h-16 bg-white shadow-sm">
-        <Image src="/icon2.png" width={32} height={32} alt="B-Tech Icon" />
+        <Link href="/">
+          <Image src="/icon2.png" width={32} height={32} alt="B-Tech Icon" />
+        </Link>
         <a>
           <AiOutlineMenu size={24} />
         </a>
