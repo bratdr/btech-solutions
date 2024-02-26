6f4f1e6bcb079be28369d14618dd6a066cbc91b6
Brata Darmawan <bratadarmawan@gmail.com>
2024-02-27 05:43:27 +0700

    update About

--- app/pages/about/index.jsx
+++ app/pages/about/index.jsx
@@ -9,18 +9,18 @@ import Link from "next/link";
 const About = () => {
   return (
     <>
-      <div className="bg-about text-white mt-14 py-14 px-6 overflow-x-scroll ">
+      <div className="mt-14 bg-about px-6 py-14 text-white" id="about">
         <div className="max-w-[325px]">
-          <h1 className="text-[32px] leading-[40px] tracking-tight font-bold ">
+          <h1 className="text-[32px] font-bold leading-[40px] tracking-tight ">
             Tentang Perusahaan
           </h1>
-          <h3 className="text-2xl leading-[40px] tracking-tight font-bold mt-4 text-[#F9CD34]">
+          <h3 className="mt-4 text-2xl font-bold leading-[40px] tracking-tight text-[#F9CD34]">
             B.Tech Solutions
           </h3>
-          <div className="text-sm font-medium mt-2 leading-loose">
+          <div className="mt-2 text-sm font-medium leading-loose">
             <p>
               B.tech adalah perusahaan yang berjalan di bidang “
-              <span className="text-[#F9CD34] font-bold">
+              <span className="font-bold text-[#F9CD34]">
                 Business solution providers
               </span>
               ”. Kami memahami bahwa setiap perusahaan memiliki tantangan yang
@@ -30,20 +30,20 @@ const About = () => {
             </p>
           </div>
         </div>
-        <div className="bg-white w-full h-[208px] mt-6 p-[1px] rounded-md">
-          <div className="w-full h-full bg-video bg-no-repeat object-cover flex items-center justify-center rounded-md">
+        <div className="mt-6 h-[208px] w-full rounded-md bg-white p-[1px]">
+          <div className="flex h-full w-full items-center justify-center rounded-md bg-video bg-no-repeat object-cover">
             <Image
               src={Play_Img}
               alt="play button"
-              className="w-[32px] h-[32px]"
+              className="h-[32px] w-[32px]"
             />
           </div>
         </div>
-        <div className="max-w-[325px] mt-10">
-          <h3 className="text-2xl leading-[40px] tracking-tight font-bold mt-4 text-[#F9CD34]">
+        <div className="mt-10 max-w-[325px]">
+          <h3 className="mt-4 text-2xl font-bold leading-[40px] tracking-tight text-[#F9CD34]">
             Dokumentasi Kegiatan
           </h3>
-          <div className="text-sm font-medium mt-2 leading-loose">
+          <div className="mt-2 text-sm font-medium leading-loose">
             <p>
               Telusuri foto-foto yang menarik dan hidupkan kembali sorotan dari
               upaya kolektif kami dalam membangun persatuan, pemberdayaan, dan
@@ -53,24 +53,24 @@ const About = () => {
         </div>
         {/* Kegiatan Galeri */}
         <div className="flex flex-col gap-4">
-          <h4 className="text-xl leading-[40px] tracking-tight font-bold mt-6">
+          <h4 className="mt-6 text-xl font-bold leading-[40px] tracking-tight">
             Galeri Kegiatan
           </h4>
           <div className="flex gap-4 overflow-x-scroll">
-            <div className="min-w-[183px] min-h-[112px]">
+            <div className="min-h-[112px] min-w-[183px]">
               <Image src={Kegiatan1} alt="Photo Kegiatan 1" />
             </div>
-            <div className="min-w-[183px] min-h-[112px]">
+            <div className="min-h-[112px] min-w-[183px]">
               <Image src={Kegiatan2} alt="Photo Kegiatan 1" />
             </div>
-            <div className="min-w-[183px] min-h-[112px]">
+            <div className="min-h-[112px] min-w-[183px]">
               <Image src={Kegiatan3} alt="Photo Kegiatan 1" />
             </div>
           </div>
           <Link
             href="/about"
-            className="w-[120px] h-8 flex justify-center items-center
-             px-4 py-2 text-sm font-bold rounded-md text-[#13194B] bg-[#F9CD34]"
+            className="flex h-8 w-[120px] items-center justify-center
+            rounded-md bg-[#F9CD34] px-4 py-2 text-sm font-bold text-[#13194B]"
           >
             Selengkapnya
           </Link>
