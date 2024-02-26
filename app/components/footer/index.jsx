ee5f11f3f58960acee74a31f8a8641cb9daa5b34
Brata Darmawan <bratadarmawan@gmail.com>
2024-02-27 05:39:16 +0700

    create Footer components

---
+++ app/components/footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-primary w-full px-6 py-16">
+        <h1 className=" text-white">AKU FOOTER</h1>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
