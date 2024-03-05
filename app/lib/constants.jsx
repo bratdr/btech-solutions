82853703e7b3667fd7366f8588033f23de0c3e10
Brata Darmawan <bratadarmawan@gmail.com>
2024-03-05 10:16:56 +0700

    added constants

---
+++ app/lib/constants.jsx
@@ -0,0 +1,45 @@
+// Import images
+import Berita1 from "../assets/image/berita/berita1.png";
+import Berita2 from "../assets/image/berita/berita2.png";
+import Berita3 from "../assets/image/berita/berita3.png";
+import Berita4 from "../assets/image/berita/berita3.png";
+
+// Define constants for carousel items
+export const carouselItems = [
+  {
+    id: 1,
+    imageSrc: Berita1,
+    title:
+      "Membaca Ulang Simpanan Nasabah di Perbankan, Ada Fenomena Makan Tabungan?",
+    full_date: "Rabu, 8 Februari 2024",
+    web_title: "membaca-ulang-simpanan-nasabah-di-perbankan",
+    year: "2024",
+    month: "02",
+    date: "08",
+  },
+  {
+    id: 2,
+    imageSrc: Berita2,
+    title: "Mengukur Potensi Valuasi Merger Grab dan GOTO Jika Benar Terjadi",
+    full_date: "Senin, 6 Februari 2024",
+    web_title:
+      "mengukur-potensi-valuasi-merger-grab-dan-goto-jika-benar-terjadi",
+    year: "2024",
+    month: "02",
+    date: "06",
+  },
+  {
+    id: 3,
+    imageSrc: Berita3,
+    title:
+      "Membaca Ulang Simpanan Nasabah di Perbankan, Ada Fenomena Makan Tabungan?",
+    date: "Selasa, 7 Februari 2024",
+  },
+  {
+    id: 4,
+    imageSrc: Berita4,
+    title:
+      "Membaca Ulang Simpanan Nasabah di Perbankan, Ada Fenomena Makan Tabungan?",
+    date: "Selasa, 7 Februari 2024",
+  },
+];
