ee1c949ef6084ac6d17d69f084b56ba20a94e985
Brata Darmawan <bratadarmawan@gmail.com>
2024-03-05 10:15:20 +0700

    added desktop viewport

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Berita from "./pages/berita";
 
 export default function Home() {
   return (
-    <main className="w-full pt-16 xl:px-32">
+    <main className="w-full pt-16 xl:flex xl:flex-col xl:items-center xl:justify-center xl:px-32">
       <div className="bg-black xl:hidden">
         <Image
           src={Hero_Img}
