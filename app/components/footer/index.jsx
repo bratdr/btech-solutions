e4770c197ed927aef8f8f64e4e35e858efc7e623
Brata Darmawan <bratadarmawan@gmail.com>
2024-03-13 10:50:37 +0700

    update Footer components

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="w-full bg-primary px-6 py-16">
-        <h1 className=" text-white">AKU FOOTER</h1>
+      <div className="w-full bg-primary px-6 py-16 xl:px-32">
+        <h1 className="font-semibold text-white">
+          ©2024 B-Tech Company, Inc. All rights reserved.
+        </h1>
       </div>
     </>
   );
